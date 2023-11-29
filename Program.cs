cef31760d31bfd689307854117eb26f1696bb111
unknown <preetivikrant91@gmail.com>
2023-11-29 14:48:53 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+namespace ExcelColumnTitleToColumnNumber
+{
+    class Program
+    { 
+    static void Main()
+    {
+        Console.Write("Enter Excel column title: ");
+        string columnTitle = Console.ReadLine().ToUpper(); // Convert to uppercase 
+        int columnNumber = ColumnTitleToColumnNumber(columnTitle);
+        Console.WriteLine($"The column number for {columnTitle} is {columnNumber}");
+
+        //solution using LINQ
+        int alternativeColumnNumber = TitleToNumberLinq(columnTitle);
+        Console.WriteLine($"(Linq) The column number for {columnTitle} is {alternativeColumnNumber}");
+    }
+
+    static int ColumnTitleToColumnNumber(string columnTitle)
+    {
+        int result = 0;
+
+        foreach (char c in columnTitle)
+        {
+            Console.WriteLine("value :" +c);
+            result = result * 26 + (c - 'A' + 1);
+        }
+
+        return result;
+    }
+
+    //  using LINQ
+    static int TitleToNumberLinq(string columnTitle)
+    {
+        return columnTitle.Aggregate(0, (result, c) => result * 26 + (c - 'A' + 1));
+    }
+    }
+}
